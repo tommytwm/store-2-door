48ff21ed3dea9bce3854a5909b3a4776d34ac586
Tommy Tho <tommythowm@gmail.com>
2020-06-14 14:00:02 -0700

    heroku init

--- src/config/databaseConfig.js
+++ src/config/databaseConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "password",
-    DB: "testdb"
+    HOST: "us-cdbr-east-05.cleardb.net",
+    USER: "b591962fd93be6",
+    PASSWORD: "94f8981c",
+    DB: "heroku_d5cd97bc013e378"
 };
