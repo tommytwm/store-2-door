0ed86f3c40c005e8c6ad6578e58566beec717698
Tommy Tho <tommythowm@gmail.com>
2020-06-16 17:10:08 -0700

    added search function for containsItem to search by requestId

--- src/controllers/containsItemController.js
+++ src/controllers/containsItemController.js
@@ -26,6 +26,14 @@ exports.get_contains_item_by_id = function (req, res) {
     });
 };
 
+exports.get_contains_item_by_request_id = function (req, res) {
+  ContainsItem.getContainsItemById(req.params.requestId, function(err, containsItem) {
+      if (err)
+        res.send(err);
+      res.json(containsItem);
+  });
+};
+
 exports.get_all_contains_items = function (req, res) {
     ContainsItem.getAllContainItems(function(err, containsItem) {
         if (err)
