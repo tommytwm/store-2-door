d2deb228a26a4e928e7aeadcf34e1db577ec8cd3
Tommy Tho <tommythowm@gmail.com>
2020-06-16 18:33:36 -0700

    updated item endpoints so that none of which conflicts

--- src/routes/api/item.js
+++ src/routes/api/item.js
@@ -1,8 +1,8 @@
 var router = require('express').Router();
 var itemController = require('../../controllers/itemController');
 
-router.get('/:itemId', itemController.get_item_by_id);
-router.get('/:storeId', itemController.get_items_by_store_id);
+router.get('/by-item/:itemId', itemController.get_item_by_id);
+router.get('/by-store/:storeId', itemController.get_items_by_store_id);
 router.get('/:supplierId', itemController.get_items_by_supplier_id);
 router.get('/items-priced-under/', itemController.get_items_below_price);
 router.get('/', itemController.get_all_items);
