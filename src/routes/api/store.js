49facf898e357c673cf4fd133988832f44af18a8
Tommy Tho <tommythowm@gmail.com>
2020-06-16 23:40:05 -0700

    added add/delete/edit and aggregate function for store model

--- src/routes/api/store.js
+++ src/routes/api/store.js
@@ -3,7 +3,13 @@ var storeController = require('../../controllers/storeController');
 
 router.get('/:storeId', storeController.get_store_by_id);
 router.get('/', storeController.get_all_stores);
+router.get('/min-items/:num', storeController.get_store_with_min_items);
+
+router.put('/update/:storeId/:name/:geoLocId', storeController.edit_store);
 
 router.post('/', storeController.create_store);
+router.post('/:name/:geoLocId', storeController.add_store);
+
+router.delete('/remove/:storeId', storeController.delete_store);
 
 module.exports = router;
